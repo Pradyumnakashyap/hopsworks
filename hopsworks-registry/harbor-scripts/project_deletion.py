0cdd788499125104cd9afe55352422344f62f792
PK <pradyumnakas@gmail.com>
2020-08-11 20:42:39 +0200

    update scripts from parent git repository

--- hopsworks-registry/harbor-scripts/project_deletion.py
+++ hopsworks-registry/harbor-scripts/project_deletion.py
@@ -37,7 +37,7 @@ def get_project_id(project_name):
 	#	print('Please verify the registry credentials!')
 def delete_repo(project_name):
 
-        repo_name="conda-image"
+        repo_name="conda_image"
         resp = requests.delete(r'https://{}/api/v2.0/projects/{}/repositories/{}'.format(harbor_host,project_name,repo_name),verify=r'/etc/docker/certs.d/{}/ca.crt'.format(harbor_host),auth=(user,password),headers={'Content-Type':'application/json'})
         if resp.status_code != 200:
                 print('Error deleting the repository with status code /api/projects {}'.format(resp.status_code))
@@ -75,4 +75,5 @@ if __name__ == '__main__':
         password= str(sys.argv[3])
         project_name=str(sys.argv[4])
         delete_project(project_name)
+        
 
